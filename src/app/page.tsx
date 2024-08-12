561b2a30846d830fdc98d7bbbf504a7191027394
Vinicius <beneditoloura@gmail.com>
2024-08-12 06:35:25 -0300

    feat: get players data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,26 @@
 import { auth } from '@/services/auth'
-import Image from 'next/image'
 import { redirect } from 'next/navigation'
+import CardPlayer from './components/CardPlayer/CardPlayer'
+import { PlayerProps } from '@/types/types'
+
+const getData = async (): Promise<PlayerProps[]> => {
+  const res = await fetch(`${process.env.API_URL}/get_users`)
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
 
 const HomePage = async () => {
   const session = await auth()
 
-  if (!session?.user) redirect('/api/auth/signin')
+  if (!session?.user) redirect('/auth')
 
-  return (
-    <>
-      <h1>{session.user.name}</h1>
-      <Image
-        src={session?.user?.image as string}
-        alt={session.user.name as string}
-        width={72}
-        height={72}
-        className="rounded-full"
-      />
-    </>
-  )
+  const data = await getData()
+
+  return <CardPlayer players={data} />
 }
 
 export default HomePage
