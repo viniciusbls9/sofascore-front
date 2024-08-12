a2c1a6036dc11801d1904defced50c15bed295e5
Vinicius <beneditoloura@gmail.com>
2024-08-12 06:35:01 -0300

    test: add auth form test

---
+++ src/app/auth/_components/auth-form.spec.tsx
@@ -0,0 +1,25 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import { AuthForm } from './auth-form'
+import '@testing-library/jest-dom'
+
+describe('AuthForm', () => {
+  test('renders the AuthForm component correctly', () => {
+    render(<AuthForm loginAction={jest.fn()} />)
+
+    expect(screen.getByText('Bem vindo')).toBeInTheDocument()
+    expect(
+      screen.getByText('Faça seu login e vote nos seus favoritos'),
+    ).toBeInTheDocument()
+    expect(screen.getByTestId('login-button')).toBeInTheDocument()
+    expect(screen.getByTestId('chrome-icon')).toBeInTheDocument()
+  })
+
+  test.only('calls loginAction when the login button is clicked', async () => {
+    const loginAction = jest.fn()
+
+    render(<AuthForm loginAction={loginAction} />)
+
+    const loginButton = screen.getByTestId('login-button')
+    fireEvent.click(loginButton)
+  })
+})
