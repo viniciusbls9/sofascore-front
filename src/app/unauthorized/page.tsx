2ad5d52881ea7fc2b139de998e7bdabc3468d2c8
Vinicius <beneditoloura@gmail.com>
2024-08-27 07:41:46 -0300

    refactor: adjust eslint rules

---
+++ src/app/unauthorized/page.tsx
@@ -0,0 +1,10 @@
+const UnauthorizedPage = () => {
+  return (
+    <h1>
+      Você ainda nâo esta cadastrado na base de dados da Copa Catarina. Peça
+      para o Rhenan cadatrar seus dados e volte a fazer o login
+    </h1>
+  )
+}
+
+export default UnauthorizedPage
